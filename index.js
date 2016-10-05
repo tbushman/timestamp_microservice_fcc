5ac07d1ddc3f35e0b28c073cfc804f21a6f4a3a5
tbushman <tbushman@pu.bli.sh>
2016-10-05 12:42:13 -0600

    separate path and body parse

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var path = require('path');
+var url = require('url');
 var bodyParser = require('body-parser');
+var http = require('http');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -12,16 +16,29 @@ app.use(bodyParser.json());
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 app.use(urlencodedParser);
 
-app.get('/', function (req, res) {
-  res.render('index', {title: 'FCC Timestamp Microservice'});
+app.get(/(.+)/, function (req, res) {
+	var dateInput = url.parse(req.url).pathname;
+	dateInput = dateInput.replace(/%20/g, " ");
+	res.render('index', {
+		title: 'FCC Timestamp Microservice',
+		response: formatDate(dateInput)
+	});
+});
+
+app.post('/', urlencodedParser, function(req, res){
+	var dateInput = req.body.date;	
+	res.render('index', {
+		title: 'FCC Timestamp Microservice',
+		response: formatDate(dateInput)
+	});
 });
 
 var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-app.post('/process_post', urlencodedParser, function(req, res){
-	var data = req.body.date;
-	var newDate = new Date(''+data+'');
+
+function formatDate(dateInput){
+	var newDate = new Date(''+dateInput+'');
 	if (newDate == NaN || newDate == "Invalid Date") {
-		var convertDate = new Date(data*1000);
+		var convertDate = new Date(dateInput*1000);
 		if (convertDate == "Invalid Date" || convertDate == NaN) {
 			response = {
 				"unix": null,
@@ -44,13 +61,9 @@ app.post('/process_post', urlencodedParser, function(req, res){
 		"unix": unixDate,
 		"natural": natural
 	}
-	console.log(JSON.stringify(response));
-	res.render('index', {
-		title: 'FCC Timestamp Microservice', 
-		response: JSON.stringify(response)
-		}
-	);
-});
+	return JSON.stringify(response);
+}
+
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
